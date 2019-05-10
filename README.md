f01399816b93e0ab1a2f929429a0c1aee9346e32
migueldoctor <mikydoc_sev@hotmail.com>
2019-05-10 16:49:10 +0200

    Commit functional version described here https://medium.com/@migueldoctor/spring-cloud-series-crea-y-configura-paso-a-paso-un-servicio-gateway-para-routing-y-balanceo-de-aaf79398d39e

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Cloud Netflix Zuul Gateway Server - Demo step by step
+
+This repo contains the code to create a Spring Cloud Netflix Zuul Gateway Server with a basic but functional configuration
+
+The step by step guide and the explanation can be found on Medium:
+
+[Spanish version](https://medium.com/@migueldoctor/spring-cloud-series-crea-un-servicio-de-registro-y-descubrimiento-con-spring-cloud-netflix-eureka-4758615ad4cb)
---
+++ src/main/java/com/kesizo/microservices/gateway/ZuulGatewayServer.java
@@ -0,0 +1,15 @@
+package com.kesizo.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulGatewayServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayServer.class, args);
+    }
+}
